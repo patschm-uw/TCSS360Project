8bce5861a2d983f1935088c469c6ae0698b2fb29
Kezeba <Kezeba@10.0.0.59>
2020-05-24 17:33:19 -0700

    ImportScreen

---
+++ SettingScreen/src/ImportScreen.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.*;
+public class ImportScreen extends JFrame {
+	/** File opener to browse directory. */
+	private JFileChooser myFileChooser;
+	/**
+	 * serialVerison
+	 */
+	public static void main(String[] args) {
+		new ImportScreen();
+	} 
+	private static final long serialVersionUID = 1L;
+	JPanel panelTop;
+	JPanel panelBottom;
+	JLabel select;
+	JTextField directoryText;
+	JButton browse;
+	JButton Ok;
+	ImportScreen() {
+		this.myFileChooser = new JFileChooser();
+		// Name Label
+		select = new JLabel();
+		select.setText("Select a File :");
+		directoryText = new JTextField(10);
+		// Buttons
+		browse = new JButton("Browse");
+		Ok = new JButton("OK");
+		panelTop = new JPanel();
+		panelBottom = new JPanel();
+		panelTop.add(select);
+		panelTop.add(directoryText);
+		panelBottom.add(Ok);
+		panelTop.add(browse);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		add(panelTop, BorderLayout.CENTER);
+		add(panelBottom, BorderLayout.AFTER_LAST_LINE);
+		setTitle("Import Setting");
+		setSize(400,200);
+		setVisible(true);
+		browse.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				myFileChooser = new JFileChooser();
+				myFileChooser.setDialogTitle("Select a File");
+				myFileChooser.showSaveDialog(null);				 
+				File f = myFileChooser.getSelectedFile();
+				String filename = f.getAbsolutePath();
+				directoryText.setText(filename);
+				int returnValue = myFileChooser.showOpenDialog(null);
+				if (returnValue == JFileChooser.APPROVE_OPTION) {
+				}
+				else {
+				}
+			}
+		});
+		Ok.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+
+			}
+
+		});
+	}	
+
+
+}	
+
+
