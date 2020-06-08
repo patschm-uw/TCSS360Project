8f641fe5f325f4a1ee85c269c9d8d84ff90fbcb2
kieutrinh04 <63382917+kieutrinh04@users.noreply.github.com>
2020-06-07 21:11:43 -0700

    waitting for Room GUI class

--- SettingScreen/src/RoomList.java
+++ SettingScreen/src/RoomList.java
@@ -1,223 +1,143 @@
-/**
- * 
- */
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSeparator;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
-/**
- * @author Hamza Shanle
- *
- */
 
-import java.awt.*;
-import java.awt.event.*;
-import java.util.ArrayList;
 
-import javax.swing.*;
-import javax.swing.event.*;
- 
-
-public class RoomList extends JPanel
-                      implements ListSelectionListener {
-    /**
-	 * deafult serial version number.
-	 */
+public class RoomList extends JPanel{
 	private static final long serialVersionUID = 1L;
-	private JList<Object> list;
-    private DefaultListModel<Object> listModel;
- 
-    private static final String addString = "Add Room";
-    private static final String deleteString = "Delete Room";
-    private JButton deleteButton;
-    private JTextField roomName;
-    private JTextField path;
-    private JLabel roomN = new JLabel("Room: ");
-    private JLabel pathN = new JLabel("Path: ");
-    private Room room;
- 
-    public RoomList() {
-        super(new BorderLayout());
- 
-        listModel = new DefaultListModel<Object>();
- 
-        //Create the list and put it in a scroll pane.
-        list = new JList<Object>(listModel);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.setSelectedIndex(0);
-        list.addListSelectionListener(this);
-        list.setVisibleRowCount(5);
-        JScrollPane listScrollPane = new JScrollPane(list);
- 
-        JButton addButton = new JButton(addString);
-        AddListener addListener = new AddListener(addButton);
-        addButton.setActionCommand(addString);
-        addButton.addActionListener(addListener);
-        addButton.setEnabled(false);
- 
-        deleteButton = new JButton(deleteString);
-        deleteButton.setActionCommand(deleteString);
-        deleteButton.addActionListener(new DeleteListener());
- 
-        roomName = new JTextField(10);
-        roomName.addActionListener(addListener);
-        roomName.getDocument().addDocumentListener(addListener);
-        
-        path = new JTextField(10);
-        path.addActionListener(addListener);
-        path.getDocument().addDocumentListener(addListener);
-        
-        room = new Room(roomName.getText(), path.getText());
-        ArrayList<Object> listOfRooms = new ArrayList<Object>();
-        listOfRooms.add(room);
+	private JList<String> list;
+    private final DefaultListModel<String> listModel;
+    private String homePath;
 
- 
-        //Create a panel that uses BoxLayout.
+    private JButton addButton; 
+    private JButton deleteButton;
+    private JButton searchButton;
+    private JTextField roomName;
+    private JLabel roomN = new JLabel("Room: ");
+    private int index;
+    File roomPath;
+    
+    public RoomList() {
+    	super(new BorderLayout());
+    	
+    	index =0;
+    	homePath = System.getProperty("user.dir") + File.separator + "HOME";
+    	roomPath = new File(homePath);
+    	
+    	roomName = new JTextField();
+        roomName.setEditable(true);
+        roomName.setSize(36, 36);
+        
+    	//Create the list and put it in a scroll pane.
+    	listModel = new DefaultListModel<>();
+    	list = listSetUp();
+        JScrollPane scrollPane = new JScrollPane(list);
+        add(scrollPane, BorderLayout.CENTER);
+        
         JPanel buttonPane = new JPanel();
-        buttonPane.setLayout(new BoxLayout(buttonPane,
-                                           BoxLayout.LINE_AXIS));
-        buttonPane.add(deleteButton);
-        buttonPane.add(Box.createHorizontalStrut(5));
-        buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
-        buttonPane.add(Box.createHorizontalStrut(5));
+        buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.X_AXIS));
+        //buttonPane.add(Box.createHorizontalStrut(5));
+        //buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
+        //buttonPane.add(Box.createHorizontalStrut(5));
         buttonPane.add(roomN);
         buttonPane.add(roomName);
-        buttonPane.add(pathN);
-        buttonPane.add(path);
+        addButton = add();
         buttonPane.add(addButton);
+        deleteButton = delete();
+        buttonPane.add(deleteButton);
+        searchButton = search();
+        buttonPane.add(searchButton);
         buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
- 
-        add(listScrollPane, BorderLayout.CENTER);
-        add(buttonPane, BorderLayout.PAGE_END);
+        add(buttonPane, BorderLayout.NORTH);
     }
- 
-    class DeleteListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            int index = list.getSelectedIndex();
-            listModel.remove(index);
- 
-            int size = listModel.getSize();
- 
-            if (size == 0) { 
-                deleteButton.setEnabled(false);
- 
-            } else { 
-                if (index == listModel.getSize()) {
-                    index--;
-                }
- 
-                list.setSelectedIndex(index);
-                list.ensureIndexIsVisible(index);
-            }
-        }
-    }
- 
-    class AddListener implements ActionListener, DocumentListener {
-        private boolean alreadyEnabled = false;
-        private JButton button;
- 
-        public AddListener(JButton button) {
-            this.button = button;
-        }
- 
-		public void actionPerformed(ActionEvent e) {
-            String name = roomName.getText();
- 
-            //User didn't type in a unique name...
-            if (name.equals("") || alreadyInList(name)) {
-                Toolkit.getDefaultToolkit().beep();
-                roomName.requestFocusInWindow();
-                roomName.selectAll();
-                return;
-            }
- 
-            int index = list.getSelectedIndex(); 
-            if (index == -1) { 
-                index = 0;
-            } else {           
-                index++;
-            }
-            room = new Room(roomName.getText(), path.getText());
- 
-            listModel.insertElementAt(room.getTitle(), index);
- 
-
-            roomName.requestFocusInWindow();
-            roomName.setText("");
- 
-
-            list.setSelectedIndex(index);
-            list.ensureIndexIsVisible(index);
-            
-        }
- 
-        protected boolean alreadyInList(String name) {
-            return listModel.contains(name);
-        }
- 
-        //Required by DocumentListener.
-        public void insertUpdate(DocumentEvent e) {
-            enableButton();
-        }
- 
-        //Required by DocumentListener.
-        public void removeUpdate(DocumentEvent e) {
-            handleEmptyTextField(e);
-        }
- 
-        //Required by DocumentListener.
-        public void changedUpdate(DocumentEvent e) {
-            if (!handleEmptyTextField(e)) {
-                enableButton();
-            }
-        }
- 
-        private void enableButton() {
-            if (!alreadyEnabled) {
-                button.setEnabled(true);
-            }
-        }
- 
-        private boolean handleEmptyTextField(DocumentEvent e) {
-            if (e.getDocument().getLength() <= 0) {
-                button.setEnabled(false);
-                alreadyEnabled = false;
-                return true;
-            }
-            return false;
-        }
-    }
- 
-    public void valueChanged(ListSelectionEvent e) {
-        if (e.getValueIsAdjusting() == false) {
- 
-            if (list.getSelectedIndex() == -1) {
-
-                deleteButton.setEnabled(false);
- 
-            } else {
-
-                deleteButton.setEnabled(true);
-            }
-        }
-    }
-    private static void createAndShowGUI() {
-        //Create and set up the window.
-        JFrame frame = new JFrame("RoomList");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Create and set up the content pane.
-        JComponent newContentPane = new RoomList();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame.setContentPane(newContentPane);
- 
-        //Display the window.
-        frame.pack();
-        frame.setVisible(true);
-    }
- 
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
+    
+    private JList<String> listSetUp() {
+    	for(String s: roomPath.list()) {
+    		listModel.add(index, s);
+    		index++;
+    	}
+        list = new JList<>(listModel);
+        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        list.addListSelectionListener(new ListSelectionListener() {
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				//need to call the room class
+			}
         });
+        return list;
     }
+    
+    private JButton add() {
+    	addButton = new JButton("Add Room");
+        addButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				File newRoom = new File(homePath + File.separator + roomName.getText());
+				if(!listModel.contains(roomName.getText())) {
+					newRoom.mkdirs();
+					listModel.add(index,roomPath.list()[index]);
+				}else {
+			         JOptionPane.showMessageDialog(null, "This file does exist");
+				}
+			}
+        });
+        return addButton; 
+    }
+    private JButton delete() {
+    	deleteButton = new JButton("Delete Room");
+        deleteButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				File newRoom = new File(homePath + File.separator + roomName.getText());
+				if(listModel.contains(roomName.getText())) {
+					newRoom.delete();
+					listModel.remove(listModel.indexOf(roomName.getText()));
+				}else {
+			         JOptionPane.showMessageDialog(null, "This file does exist");
+				}
+			}
+        });
+        return deleteButton;
+    }
+    
+    private JButton search() {
+    	searchButton = new JButton("Search Room");
+        searchButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(listModel.contains(roomName.getText())) {
+		            int index = listModel.indexOf(roomName.getText());
+		            list.setSelectedIndex(index);
+		           
+		        } else {
+		            list.clearSelection();
+			        JOptionPane.showMessageDialog(null, "File is not found");
+			         
+		        }
+			}
+        });
+        return searchButton;
+    }
+
+
+	
 }
