c1836280d54061bdb07e3bf913f5a8d828aef13e
kieutrinh04 <63382917+kieutrinh04@users.noreply.github.com>
2020-06-11 21:19:00 -0700

    Done

--- SettingScreen/src/Launcher.java
+++ SettingScreen/src/Launcher.java
@@ -9,16 +9,13 @@ import java.io.IOException;
 public class Launcher {
     
     public static void main(String[] args) throws IOException {
-        RoomList rlist = new RoomList();
+       
         //create main folder
         String path = System.getProperty("user.dir")+"\\HOME";
         File dir = new File(path);
         if(!dir.exists()) {
             dir.mkdir();
         }
-        path = (String) System.getProperty("user.dir") + File.separator + "HOME";
-        path = path.replace(File.separator, "\\\\");
-        
-        String roomPath = path + File.separator + rlist.getList().getSelectedValue();
+        RoomList rlist = new RoomList();
     }
 }
