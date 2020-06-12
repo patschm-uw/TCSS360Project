53b24e8751dcb609d7732bae6c3eaccb6c7803b5
kieutrinh04 <63382917+kieutrinh04@users.noreply.github.com>
2020-06-11 20:18:15 -0700

    final edition. missing javadoc

--- Project/src/Items.java
+++ Project/src/Items.java
@@ -11,7 +11,7 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 /**
- * Items class that lists out all the items from the folder path.
+ * I tems class list out all the item from the folder path.
  * @author Kieu Trinh
  * @Team Ocelot
  * @Item version 
@@ -28,7 +28,7 @@ public class Items {
     /**
      * The list string contain all the name of the item in the folder
      */
-    private List<String> fileContent;
+    List<String> fileContent;
     /**
      * file the folder path
      */
@@ -50,9 +50,12 @@ public class Items {
      * @return list fileContent
      */
     public List<String> getItemList(){
-        for(String content : file.list()) {
-            fileContent.add(content);
-        }
+    	if(file.list().length > 0) {
+    		for(String content : file.list()) {
+                fileContent.add(content);
+            }
+    	}
+        
         return fileContent;
     }
     /**
@@ -129,6 +132,7 @@ public class Items {
         int count = 0;
         JFrame frame = new JFrame();
         String home = System.getProperty("user.home");
+        System.out.println(home);
         String downloads = home + "\\Downloads\\";
         for(String s : this.fileContent) {
             uploadScreen.copyFile(path + "\\" + s, downloads + s);
--- Project/src/RoomView.java
+++ Project/src/RoomView.java
@@ -1,26 +1,32 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.File;
-
-import javax.swing.DefaultListModel;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JList;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
 /**
  * showing all the file in selected folder/directory
  * @author Kieu Trinh, Hamza Shanle
  * @Team Ocelot
  * @Item version 
  */
+import java.awt.BorderLayout;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListSelectionModel;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+
 public class RoomView extends JFrame
                       implements ActionListener {
 	
@@ -31,27 +37,49 @@ public class RoomView extends JFrame
 	private String path;
     private JButton deleteButton;
     private JButton addButton;
-    private JButton searchButton;
+    private JButton download;
     private JButton backButton;
     private JList<String> list;
     private DefaultListModel<String> listModel;
     private Dimension screenSize;
     //private File roomPath;
     private Items roomItem;
+    private int index;
     
     public RoomView(String path, String name) {
+    index = 0;
     this.path = path;
     roomItem = new Items(path, name); 
     //roomPath = new File(path);
 	listModel = new DefaultListModel<String>();
     for(String item : roomItem.getItemList()) {
     	listModel.addElement(item);
+    	//index++;
     }
     //Create the list and put it in a scroll pane.
     list = new JList<String>(listModel);
     list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
     list.setSelectedIndex(0);
-    //list.addListSelectionListener((ListSelectionListener) this);
+    list.addListSelectionListener(new ListSelectionListener() {
+
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			
+			
+		}
+    	
+    });
+    list.addMouseListener(new MouseAdapter() {
+    	public void mouseClicked(MouseEvent mouseEvent) {
+    		if(mouseEvent.getClickCount() == 2) {
+    			try {
+    				Desktop.getDesktop().open(new File(path + File.separator + list.getSelectedValue()));
+    			} catch (IOException e1) {
+    				e1.printStackTrace();
+    			}
+    		}
+    	}
+    });
     list.setVisibleRowCount(5);
     JScrollPane listScrollPane = new JScrollPane(list);
     
@@ -59,30 +87,27 @@ public class RoomView extends JFrame
     deleteButton.addActionListener(this);
     addButton = new JButton("Add Item");
     addButton.addActionListener(this);
-    //searchButton.addActionListener(this);
     backButton = new JButton("Back to main page");
     backButton.addActionListener(this);
+    download = new JButton("Download");
+    download.addActionListener(this);
     
     JPanel buttonPane = new JPanel();
-    //buttonPane.setLayout(new BoxLayout(buttonPane,BoxLayout.LINE_AXIS));
+   
     
     GridLayout layout = new GridLayout();
     layout.setHgap(25);
-    //buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.X_AXIS));
     buttonPane.setLayout(layout);
     buttonPane.add(addButton);
     buttonPane.add(deleteButton);
-    //buttonPane.add(Box.createHorizontalStrut(5));
-    //buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
-    //buttonPane.add(Box.createHorizontalStrut(5));
-    
-    //buttonPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+    buttonPane.add(download);
     buttonPane.add(backButton);
     
     screenSize = Toolkit.getDefaultToolkit().getScreenSize();
     setSize(screenSize.width, screenSize.height);
-    add(listScrollPane, BorderLayout.CENTER);
+    
     add(buttonPane, BorderLayout.PAGE_END);
+    add(listScrollPane, BorderLayout.CENTER);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     setVisible(true);
     }
@@ -90,31 +115,26 @@ public class RoomView extends JFrame
     public void actionPerformed(ActionEvent e)
     {
         if (e.getSource() == addButton) {
-        	File filePath = new File(path);
-        	int index = filePath.list().length;
+        	setVisible(false);
         	new uploadScreen();
-        	listModel.add(listModel.getSize(), filePath.list()[index]);
         }
         if (e.getSource() == deleteButton) {
         	roomItem.deleteFile(list.getSelectedValue());
         	listModel.remove(listModel.indexOf(list.getSelectedValue()));
         }
+        if(e.getSource() == download) {
+        	try {
+				roomItem.downloadItem();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+        }
         
         if(e.getSource() == backButton) {
         	this.setVisible(false);
-        	HomeScreen home = new HomeScreen();
+        	RoomList home = new RoomList();
         	home.setVisible(true);
         }
-        /*if(e.getSource() == searchButton) {
-        	if(listModel.contains(roomName.getText())) {
-	            int index = listModel.indexOf(roomName.getText());
-	            listModel.set(0, listModel.get(index));
-	        } else {
-	            
-		        JOptionPane.showMessageDialog(null, "File is not found");
-		         
-	        }
-        }*/
     }
     
     public void valueChanged(ListSelectionEvent e) {
@@ -130,13 +150,5 @@ public class RoomView extends JFrame
             }
         }
     }
-    
-    
- 
-   /* public static void main(String[] args) {
-        RoomView room = new RoomView("C:\\Users\\Kelly Trinh\\git\\TCSS360Project\\SettingScreen\\HOME\\TEST","TEST");
-    	
-    }*/
-    
 
 }
